48cf53b28afaa09478aeed2bb49d187cc12846a9
Constant AÏDAM <109681705+caidam@users.noreply.github.com>
2025-06-09 22:39:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
@@ -50,10 +52,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-# api info
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
