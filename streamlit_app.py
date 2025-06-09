a578e8a55329624acb27c7e19c0c66791c8b1667
Constant AÏDAM <109681705+caidam@users.noreply.github.com>
2025-06-09 22:37:20 +0200

    implemented loading api data into st dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,7 @@ if ingredients_list:
 # api info
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
